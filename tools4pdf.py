b5bb27420509164cfd69c6f708404e4588df722d
Mick Kirkegaard <mickkn@gmail.com>
2022-08-11 21:27:35 +0200

    Update

--- tools4pdf.py
+++ tools4pdf.py
@@ -1,18 +1,19 @@
 import os
 import argparse
 import img2pdf
-import ocrmypdf
 
 
 image_types = (".jpg", ".jpeg")
 
 
-def parser():
+def arg_parser():
 
+    """Parser function to get all the arguments
+
+    Returns:
+        Argument parser
     """
-    @brief    Parser function to get all the arguments
-    """
-    
+
     description = ""
 
     # Construct the argument parse and parse the arguments
@@ -25,59 +26,81 @@ def parser():
                         help="Do OCR on document")
     parser.add_argument("--ocr_only", action='store_true',
                         help="Do OCR on input document")
+    parser.add_argument("-l", "--language", type=str, default="eng",
+                        help="OCR language from Tesserect")
     print('\n' + str(parser.parse_args()) + '\n')
 
     return parser.parse_args()
 
 
-def create_pdf(args):
+def create_pdf(arguments):
+
+    """Create images to pdf
+
+    Args:
+        arguments: argument parser
+
+    Returns:
 
     """
-    @brief    Create images to pdf
-    """
-    
-    imagelist = []                                                 # Contains the list of all images to be converted to PDF.
 
-    folder = args.inputs
-    output_file = args.output                                  
+    # Contains the list of all images to be converted to PDF.
+    image_list = []
+
+    folder = arguments.inputs
 
     # Find files
-    for dirpath, dirnames, filenames in os.walk(folder):
+    for dir_path, _, filenames in os.walk(folder):
         for filename in [f for f in filenames if f.endswith(image_types)]:
-            full_path = os.path.join(dirpath, filename)
-            imagelist.append(full_path)
+            full_path = os.path.join(dir_path, filename)
+            image_list.append(full_path)
 
     # Sort files
-    imagelist.sort()                                               # Sort the images by name.
-    for i in range(0, len(imagelist)):
-        print(imagelist[i])
+    # Sort the images by name.
+    image_list.sort()
+    for i in range(0, len(image_list)):
+        print(image_list[i])
 
     # Convert images to PDF
-    with open(args.output + ".pdf","wb") as f:
-        f.write(img2pdf.convert(imagelist, dpi=600))
+    with open(arguments.output + ".pdf", "wb") as f:
+        f.write(img2pdf.convert(image_list))
 
-    if args.ocr:
-        create_ocr(args.output + ".pdf")
+    if arguments.ocr:
+        create_ocr(arguments.output + ".pdf", arguments)
 
 
-def create_ocr(file):
+def create_ocr(file, arguments):
 
+    """Add OCR to document
+
+    Args:
+        file: file to work on
+        arguments: argument parser
+
+    Returns:
+        None
     """
-    @brief    Add OCR to document
-    """
-    
-    ocrmypdf.ocr(language="dan", progress_bar=True, use_threads=True, deskew=True, input_file=file, output_file=file.replace(".pdf", "_orc.pdf"))
+
+    filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+
+    # If file is only filename, assume that the file is in root dir.
+    if os.sep not in file:
+        file = filepath
+
+    command = f"ocrmypdf -l {arguments.language} --deskew --output-type pdf {file} {file.replace('.pdf', '_ocr.pdf')}"
+     
+    res = os.system(command)
+ 
+    print("Returned Value: ", res)
 
 
 if __name__ == "__main__":
 
-    """
-    @brief    Main entry point
-    """
+    """ Main """
 
-    args = parser()
+    args = arg_parser()
 
     if not args.ocr_only:
         create_pdf(args)
     elif args.ocr_only:
-        create_ocr(args.inputs)
+        create_ocr(args.inputs, args)
